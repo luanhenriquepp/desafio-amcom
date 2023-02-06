0db997fb21ca7d36e806a5931ae24a6bf7052b10
Luan Henrique <luan.henriqueepp@gmail.com>
2023-02-06 18:05:41 -0300

    inicializando a casca do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  iconUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYg4F1I7cE-M9thHdrM2ZvwBz9D6RTul0o9A&usqp=CAU'
   title = 'desafio-amcom';
 }
